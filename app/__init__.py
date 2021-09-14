f48e17a33a79176a1fe1674520ee2768f5987011
Jude Molloy <JudeMolloy07@hotmail.com>
2021-09-14 20:24:31 +0100

    Getting ready for production.

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,12 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_admin import Admin
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
@@ -18,6 +20,7 @@ from app.models.company import Company
 from app.models.usage_period import UsagePeriod
 from app.models.payment_link import PaymentLink
 from app.models.order import Order
+from app.models.enquiry import Enquiry
 
 from app.admin import ProtectedAdminHomeView, AdminView, ModelView
 
@@ -34,6 +37,7 @@ admin.add_view(ModelView(Company, db.session, endpoint='companies'))
 admin.add_view(ModelView(UsagePeriod, db.session, endpoint='usage-periods'))
 admin.add_view(ModelView(PaymentLink, db.session, endpoint='payment-links'))
 admin.add_view(ModelView(Order, db.session, endpoint='orders'))
+admin.add_view(ModelView(Enquiry, db.session, endpoint='enquiries'))
 
 
 from app import routes, payment_routes, admin_routes, models, errors
